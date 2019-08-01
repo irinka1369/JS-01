12d6edc122f6f269a37537795c4918af3e1e475c
irinka1369 <50370378+irinka1369@users.noreply.github.com>
2019-08-01 16:00:00 +0300

    Create js-02.js

---
+++ js-02.js
@@ -0,0 +1,11 @@
+1. https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args);
+    };
+};
+
+2. http://www.codewars.com/kata/571effabb625ed9b0600107a
+
+
